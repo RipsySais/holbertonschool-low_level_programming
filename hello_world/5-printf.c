4d515aeb995a15c60280b68952b8da5df0a7cc59
RipsySais <cysseahmed@gmail.com>
2024-10-24 13:19:02 +0200

    Script5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - point d'netrée
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	prntf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
