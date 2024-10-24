6f8d5eff23a600bfac0a2a0957fc38034b59fbba
RipsySais <cysseahmed@gmail.com>
2024-10-24 13:02:12 +0200

    Script4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - point d'entrée
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
