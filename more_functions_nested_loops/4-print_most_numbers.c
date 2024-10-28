34834f9e0c72eb0ed56b60e3962c5e7f71c940ae
RipsySais <cysseahmed@gmail.com>
2024-10-28 14:37:01 +0100

    Script4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_number - fonction qui imprime les nombres, de 0 à 9,
+ * suivis d'une nouvelle ligne.
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
+	}
+
+	_putchar ('\n');
+}
