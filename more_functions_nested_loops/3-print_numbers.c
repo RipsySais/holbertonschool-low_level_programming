9b22e43e3d59fa819b59b1eb52ed11199c38f404
RipsySais <cysseahmed@gmail.com>
2024-10-28 12:39:14 +0100

    Script3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.c"
+#include <stdio.h>
+
+/**
+ * print_numbers - fonction qui imprime les nombres, de 0 à 9
+ * , suivis d'une nouvelle ligne.
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
