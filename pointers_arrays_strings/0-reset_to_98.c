a6c0ac6e40faa47b9f77df6b624eb215acd83fc3
RipsySais <cysseahmed@gmail.com>
2024-10-29 23:37:48 +0100

    Script0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - met a jour la valeur pointé par le pointeur à 98
+ *@n: pointeur vers l'entier à mettre à jour
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
