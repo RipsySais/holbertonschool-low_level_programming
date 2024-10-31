1e95b17761f45f1e607d5cfd14b95936822f44ce
RipsySais <cysseahmed@gmail.com>
2024-10-31 13:46:27 +0100

    Script100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - une fonction qui convertit une chaîne en entier.
+ * @s: pointeur vers la chaine de caractère
+ * Return: returne l'entier converti de la chaine
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s)
+	{
+		if (*s == '_')
+		{
+			sign = -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			result = result * 10 + (*s - '0');
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (result * sign);
+
+}
