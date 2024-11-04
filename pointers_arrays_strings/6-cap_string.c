56e48346ac1408e910577b989a3d47d595454285
RipsySais <cysseahmed@gmail.com>
2024-11-04 11:55:37 +0100

    Script6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - une fonction qui met en majuscule tous les mots d'une chaîne.
+ * @str: pointeur vers la chaine de caractère
+ * Return: pointeur vers la chaine résultante
+ */
+
+char *cap_string(char *str)
+{
+	char *ptr = ptr;
+	int capitalize_next = 1;
+
+	while (*ptr != '\0')
+	{
+		if (capitalize_next && *ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 32;
+		}
+		if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' ||
+			*ptr == ',' || *ptr == ';' || *ptr == '.' ||
+			*ptr == '!' || *ptr == '?' || *ptr == '"' ||
+			*ptr == '(' || *ptr == ')' || *ptr == '{' ||
+			*ptr == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			capitalize_next = 0;
+		}
+		ptr++;
+	}
+	return (str);
+}
