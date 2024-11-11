d37247a7efdd3bab9b3716ee6637b0e0aef56d64
RipsySais <cysseahmed@gmail.com>
2024-11-11 19:06:33 +0100

    Script0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - une fonction qui imprime une chaîne,
+ * suivie d'une nouvelle ligne.
+ * @s: La chaine de caractère à imprimer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
