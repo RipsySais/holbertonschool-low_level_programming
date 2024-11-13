1d41a72aef46eac1b1c4e08a0ce093142201406f
RipsySais <cysseahmed@gmail.com>
2024-11-13 19:07:03 +0100

    Script0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,13 +19,19 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(char) * size);
 
 	if (size == 0)
+	{
 		return (NULL);
+	}
 
 	if (array == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
+	{
 		array[i] = c;
+	}
 
 	return (array);
 }
