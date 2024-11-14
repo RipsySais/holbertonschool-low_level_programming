a8a0eaca6791ca2d447d2f8a9eccc0d13ce7b3db
RipsySais <cysseahmed@gmail.com>
2024-11-14 13:05:01 +0100

    Script4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,6 +13,12 @@ int main(int argc, char **argv)
 	int somme = 0;
 	int i, j;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -23,7 +29,7 @@ int main(int argc, char **argv)
 				return (1);
 			}
 		}
-		somme = somme + atoi(argv[i]);
+		somme += atoi(argv[i]);
 	}
 	printf("%d\n", somme);
 	return (0);
