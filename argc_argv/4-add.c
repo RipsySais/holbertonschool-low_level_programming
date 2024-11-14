1f6d9245678660daaf89ef60c52e72fb585e5f71
RipsySais <cysseahmed@gmail.com>
2024-11-14 12:35:41 +0100

    Script4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,54 +1,30 @@
 #include <stdlib.h>
-#include <ctype.h>
 #include <stdio.h>
 
 /**
- * is_number - Vérifier si la chaine de caractère est un nombre
- * @str: la chaine de caractère à vérifier
- * Return: 1 si la chaine est un nombre sinon 0
+ * main - un programme qui additionne des nombres positifs.
+ * @argc: le nombre d'argument passés au programme
+ * @argv: un tableau de chaine de caractère
+ * Return: 0
  */
 
-int is_number(char *str)
+int main(int argc, char **argv)
 {
-	int i;
+	int somme = 0;
+	int i, j;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(str[i]))
+		for (j = 0; j < argv[i][j]; j++)
 		{
-			return (0);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("error\n");
+				return (1);
+			}
 		}
+		somme = somme + atoi(argv[i]);
 	}
-	return (1);
-}
-
-/**
- * main - programme qui additionne des nombres positifs passés en argument
- * @argc: le nombre d'arguments passés au programme
- * @argv: un tableau de chaine de caractères représentant les arguments
- * Return: 0 si le programme s'exécute corretement, 1 en cas d'erreur
- */
-
-int main(int argc, char *argv[])
-{
-	int sum = 0;
-	int i;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	for (i = 0; i < argc; i++)
-	{
-		if (!is_number(argv[i]))
-		{
-			printf("Erreur\n");
-			return (1);
-		}
-		sum = sum + atoi(argv[i]);
-	}
-	printf("%d\n", sum);
+	printf("%d\n", somme);
 	return (0);
 }
