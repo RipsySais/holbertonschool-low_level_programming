42f26257167037380c7c64024b1f1cf9f1fa1d9e
RipsySais <cysseahmed@gmail.com>
2024-11-18 14:23:47 +0100

    Script4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -51,40 +51,39 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *name_copy;
-	char *owner_copy;
 
-	/* Allouée de la mémoire pour la nouvelle structure dog_t */
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+		return (NULL);
+	}
+
 	new_dog = malloc(sizeof(dog_t));
+
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
 
-	/* Alloue de la mémoire et copier le nom */
-	name_copy = malloc(strlen(name) + 1);
-	if (name_copy == NULL)
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 
-	strcpy(name_copy, name);
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 
-	/* allouée de la mémoire et copier le propriétaire */
-	owner_copy = malloc(strlen(owner) + 1);
-	if (owner_copy == NULL)
+	if (new_dog->owner == NULL)
 	{
-		free(name_copy);
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-	strcpy(owner_copy, owner);
 
-	/* Initialiser la structure */
-	new_dog->name = name_copy;
+	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
-	new_dog->owner = owner_copy;
+	new_dog->owner = _strcpy(new_dog->owner, owner);
 
 	return (new_dog);
 }
