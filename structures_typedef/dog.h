0ee76aef75442ec1745809bfd2678e96355e097c
RipsySais <cysseahmed@gmail.com>
2024-11-18 11:07:14 +0100

    Script0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - la structure du dog
+ * @name: nom dog
+ * @age: age dog
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
