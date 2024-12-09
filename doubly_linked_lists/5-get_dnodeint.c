d634e5e0b1397ee8fd7c393a020b689fe9a5c3d4
RipsySais <cysseahmed@gmail.com>
2024-12-09 12:09:26 +0100

    Script5

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - renvoie le n-ième
+ * noeud d'une liste chainée dlistint_t
+ * @head: pointeur vers le début de la liste
+ * @index: index du noeud, à partir de 0
+ * Return: pointeur vers le noeud à l'index spécifié,
+ * ou NULL si le noeud n'existe pas
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			return (NULL);
+		}
+		head = head->next;
+		count++;
+	}
+
+	return (NULL);
+}
