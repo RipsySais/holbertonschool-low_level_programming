321489671e0be3d5a568ef170afc26ef6d6ba21a
RipsySais <cysseahmed@gmail.com>
2024-12-11 15:26:47 +0100

    Script1

---
+++ bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - afficher un caractère
+ * @c: le caractère à afficher
+ * Return: le caractère à imprimer
+ */
+
+int _putchar(char c)
+{
+	return putchar(c);
+}
