844eda4725b8ec5e94ccf520396657688739ea4d
RipsySais <cysseahmed@gmail.com>
2024-12-13 00:32:50 +0100

    Script0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,30 +11,41 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_read = 0, bytes_written = 0;
+	ssize_t read_file, write_file;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	buffer = malloc(letters);
+	if (buffer == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_file = read(fd, buffer, letters);
+	if (read_file == -1)
 	{
 		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	write_file = write(STDOUT_FILENO, buffer, read_file);
+	if (write_file == -1 || write_file != read_file)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
 	free(buffer);
 	close(fd);
 
-	if (bytes_written != bytes_read)
-		return (0);
-
-	return (bytes_written);
+	return (write_file);
 }
