0c1dbb8353404b2a548813c512cf6a1c24d0d21c
RipsySais <cysseahmed@gmail.com>
2024-12-13 01:01:14 +0100

    Script1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - crée un fichier
+ * @filename: nom du fichier à créer
+ * @text_content: chaine terminé par NULL à écrire dans le fichier
+ * Return: 1 en cas de succès ou -1 en cas d'échec
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	ssize_t write_file = 0;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (write_file == -1 || write_file != len)
+	{
+		close(file);
+		return (-1);
+	}
+
+	close(file);
+
+	return (1);
+}
