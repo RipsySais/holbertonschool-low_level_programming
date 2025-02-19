e0273ba8bee0f3801be75e4bd8c63fd7fd553eab
RipsySais <cysseahmed@yahoo.fr>
2025-02-19 16:12:15 +0000

    change

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
-/**
- * main - imprime tous les nombres de base 16
- * en minuscules, suivis d'une nouvelle ligne.
- * Return: 0 (sucess)
- */
-
 int main(void)
 {
-	 char ch;
+    char ch;
 
-	 for (ch = '0'; ch <= '9'; ch++)
-		 putchar(ch);
+    for (ch = '0'; ch <= '9'; ch++)
+    {
+        putchar(ch);
+    }
+    for (ch = 'a'; ch <= 'f'; ch++)
+    {
+        putchar(ch);
+    }
+    putchar('\n');
 
-	 for (ch = 'a'; ch <= 'f'; ch++)
-		 putchar(ch);
-
-	 putchar('\n');
-
-	 return (0);
+    return (0);
 }
