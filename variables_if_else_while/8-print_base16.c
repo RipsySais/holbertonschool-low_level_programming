c65ef81a4a4145f0ca6a9ced17586a4a02d5ad40
RipsySais <cysseahmed@yahoo.fr>
2025-02-19 16:09:31 +0000

    print

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,11 +10,13 @@ int main(void)
 {
 	 char ch;
 
-    for (ch = '0'; ch <= '9'; ch++)
-        putchar(ch);
-    for (ch = 'a'; ch <= 'f'; ch++)
-        putchar(ch);
-    putchar('\n');
+	 for (ch = '0'; ch <= '9'; ch++)
+		 putchar(ch);
 
-    return (0);
+	 for (ch = 'a'; ch <= 'f'; ch++)
+		 putchar(ch);
+
+	 putchar('\n');
+
+	 return (0);
 }
