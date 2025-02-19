0a2fd898648989b30aba5228db35a90f6382b154
RipsySais <cysseahmed@yahoo.fr>
2025-02-19 18:05:13 +0000

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Imprime _putchar suivi d'une nouvelle ligne
+ *
+ * Description: Utilise putchar pour afficher le texte
+ * Retourne: 0 (Succès)
+ */
+int main(void)
+{
+    putchar('_');
+    putchar('p');
+    putchar('u');
+    putchar('t');
+    putchar('c');
+    putchar('h');
+    putchar('a');
+    putchar('r');
+    putchar('\n');
+
+    return (0);
+}
