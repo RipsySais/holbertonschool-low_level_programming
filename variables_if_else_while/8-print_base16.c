610fa031775091f3f6c5bc1fb4f5b4486752e521
RipsySais <cysseahmed@yahoo.fr>
2025-02-19 16:23:31 +0000

    print

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Affiche tous les nombres de base en 16 en minuscule
+ * Description: Utilis√ putchar pour afficher les caract√res
+ * Return: 0 (succ√s)
+ */
+
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = '0'; ch <= '9'; ch++)
-    {
-        putchar(ch);
-    }
-    for (ch = 'a'; ch <= 'f'; ch++)
-    {
-        putchar(ch);
-    }
-    putchar('\n');
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
 
-    return (0);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
 }
