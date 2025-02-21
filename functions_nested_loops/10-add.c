c0128c91176ae21f49be2e42cacd0e9dec29dcd7
RipsySais <cysseahmed@gmail.com>
2025-02-21 01:13:06 +0100

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - additionne deux entiers
+ * @a: premier entier
+ * @b: deuxième entier
+ * Return: Résultat de l'addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
