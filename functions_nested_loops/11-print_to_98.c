db124d3f4863cc4b301f6ea185f10057a25a6a41
RipsySais <cysseahmed@gmail.com>
2025-02-23 21:27:46 +0100

    print_to

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,49 @@
  */
 
 void print_to_98(int n)
-{
-	int i;
+{	int i;
 
 	if (n < 98)
 	{
 		for (i = n; i < 98; i++)
 		{
-			printf("%d, ", 98);
+			print_number(i);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else if (n > 98)
 	{
 		for (i = n; i > 98; i--)
 		{
-			printf("%d, ", i);
+			print_number(i);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
-	printf("98\n");
+	print_number(98);
+	_putchar('\n');
+}
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - imprime un nombre en entier
+ * @n: le nombre à imprimer
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
